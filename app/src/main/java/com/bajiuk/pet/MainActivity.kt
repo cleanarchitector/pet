90875810045cd685f29bbbda43732e84a77049ed
cleanarchitector <bbajiuk@gmail.com>
2018-09-26 00:43:00 +0300

    Basic api added

--- app/src/main/java/com/bajiuk/pet/MainActivity.kt
+++ app/src/main/java/com/bajiuk/pet/MainActivity.kt
@@ -2,11 +2,48 @@ package com.bajiuk.pet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bajiuk.pet.network.BashApi
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://umorili.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(httpClient.build())
+                .build()
+
+        val api = retrofit.create(BashApi::class.java)
+        disposable = api.get()
+                .flatMapIterable { it }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe ({ Log.i("Test", it.text) },
+                        { it.printStackTrace()},
+                        { })
+
     }
 }
